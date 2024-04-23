c98597ea52f1625d79a6a014f8b32ce35cfc3cab
Bruno <23731360+onvrb@users.noreply.github.com>
2024-04-23 01:48:30 +0100

    Create test_data.py

---
+++ tests/test_data.py
@@ -0,0 +1,29 @@
+import parent
+from src.data import MullvadData
+from json import load
+from differ import MullvadDiff
+
+
+class TestMullvadData(MullvadData):
+    def __load_test_data(self):
+        with open('tests/snapshots/2022-01-01.json', 'r') as f:
+            return load(f)
+
+    def test_update(self):
+        old_data = self.data
+        self.data = self.__load_test_data()
+        differential = MullvadDiff(old_data, self.data)
+        return differential.gen_changes()
+
+def main():
+    mv = TestMullvadData('https://api.mullvad.net/www/relays/all/')
+    changes = mv.test_update()
+    # if not changes:
+    #     pass
+    for change in changes:
+        print(change)
+    print(changes)
+
+
+if __name__ == '__main__':
+    main()
